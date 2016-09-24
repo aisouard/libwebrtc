5590dfbadcd065900270055c8a9518ff35e2501a
Axel Isouard <axel@isouard.fr>
2016-09-24 13:52:24 +0200

    Define and use macros, set install directory paths

---
+++ CMakeModules/Common.cmake
@@ -0,0 +1,28 @@
+set(LIBWEBRTC_ENV_COMMAND ${CMAKE_COMMAND} -E env \"PATH=${CMAKE_SOURCE_DIR}/Dependencies/depot_tools:$ENV{PATH}\")
+
+macro(add_libwebrtc_command
+        ARG_NAME
+        ARG_OUTPUT
+        ARG_COMMAND
+        ARG_WORKING_DIRECTORY
+        ARG_COMMENT
+)
+    set (ARG_DEPENDENCIES ${ARGN})
+
+    add_custom_command(
+            OUTPUT  ${ARG_OUTPUT}
+            COMMAND ${ARG_COMMAND}
+            WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
+            COMMENT ${ARG_COMMENT}
+    )
+
+    add_custom_target(
+            ${ARG_NAME} ALL
+            DEPENDS ${ARG_OUTPUT}
+    )
+
+    list(LENGTH ARG_DEPENDENCIES NUM_ARG_DEPENDENCIES)
+    if (${NUM_ARG_DEPENDENCIES} GREATER 0)
+        add_dependencies(${ARG_NAME} ${ARG_DEPENDENCIES})
+    endif ()
+endmacro()
