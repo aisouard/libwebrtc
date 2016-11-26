2bbdf4e794c1a0a04f39e49fc78459952cb503e9
Axel Isouard <axel@isouard.fr>
2016-11-26 17:45:35 +0100

    CMake: Make everything working under Windows

--- CMakeModules/FindDepotTools.cmake
+++ CMakeModules/FindDepotTools.cmake
@@ -1,6 +1,12 @@
-find_program(DEPOTTOOLS_GCLIENT_EXECUTABLE
-             NAMES gclient gclient.bat
-             PATHS ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/depot_tools)
+if (WIN32)
+  find_program(DEPOTTOOLS_GCLIENT_EXECUTABLE
+               NAMES gclient.bat
+               PATHS ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/depot_tools)
+else (WIN32)
+  find_program(DEPOTTOOLS_GCLIENT_EXECUTABLE
+               NAMES gclient
+               PATHS ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/depot_tools)
+endif (WIN32)
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(DepotTools
