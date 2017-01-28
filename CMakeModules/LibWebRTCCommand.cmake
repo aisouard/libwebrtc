9392f069e42116c7ea0c5d1acceda9c5de66d790
Axel Isouard <axel@isouard.fr>
2017-01-28 11:32:47 +0100

    Major refactoring

---
+++ CMakeModules/LibWebRTCCommand.cmake
@@ -0,0 +1,23 @@
+include(CMakeParseArguments)
+
+function(libwebrtc_command)
+  set(ONE_VALUE_ARGS NAME COMMENT DEPENDS WORKING_DIRECTORY)
+  set(MULTI_VALUE_ARGS COMMAND)
+  cmake_parse_arguments(COMMAND "" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}" ${ARGN} )
+
+  set(CMF_DIR ${CMAKE_BINARY_DIR}/CMakeFiles)
+  set(STAMP_FILE "${CMF_DIR}/${COMMAND_NAME}-complete")
+
+  add_custom_command(
+      OUTPUT ${STAMP_FILE}
+      COMMENT ${COMMAND_COMMENT}
+      COMMAND ${PREFIX_EXECUTE} ${COMMAND_COMMAND}
+      COMMAND ${CMAKE_COMMAND} -E touch ${STAMP_FILE}
+      WORKING_DIRECTORY ${COMMAND_WORKING_DIRECTORY}
+      VERBATIM
+  )
+
+  add_custom_target(${COMMAND_NAME} ALL DEPENDS ${STAMP_FILE})
+
+  add_dependencies(${COMMAND_NAME} ${COMMAND_DEPENDS})
+endfunction()
