29b43339045cb28c31fa1ed6b9dab165ae84fc05
Axel Isouard <axel@isouard.fr>
2017-02-05 06:14:42 -0800

    Enhance FindDepotTools.cmake, include it before looking for Python

--- CMakeModules/FindDepotTools.cmake
+++ CMakeModules/FindDepotTools.cmake
@@ -1,12 +1,14 @@
-if (WIN32)
-  find_program(DEPOTTOOLS_GCLIENT_EXECUTABLE
-               NAMES gclient.bat
-               PATHS ${CMAKE_CURRENT_SOURCE_DIR}/depot_tools)
-else (WIN32)
-  find_program(DEPOTTOOLS_GCLIENT_EXECUTABLE
-               NAMES gclient
-               PATHS ${CMAKE_CURRENT_SOURCE_DIR}/depot_tools)
-endif (WIN32)
+find_program(DEPOTTOOLS_GCLIENT_EXECUTABLE
+             NAMES gclient gclient.bat
+             PATHS ${CMAKE_CURRENT_SOURCE_DIR}/depot_tools)
+
+find_path(DEPOTTOOLS_PATH
+          NAMES gclient gclient.py gclient.bat
+                gn gn.py gn.bat
+                ninja ninja.exe ninja-linux32 ninja-linux64 ninja-mac
+                download_from_google_storage download_from_google_storage.bat
+                download_from_google_storage.py
+          HINTS ${CMAKE_CURRENT_SOURCE_DIR}/depot_tools)
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(DepotTools
