282570038ccbfb07b20f73f465433fea379f9650
Axel Isouard <axel@isouard.fr>
2017-02-06 19:13:57 +0100

    Move options code to a separate CMake module

---
+++ CMakeModules/Options.cmake
@@ -0,0 +1,27 @@
+#
+# Options, flags
+option(BUILD_TESTS "Build test binaries" OFF)
+set(NINJA_ARGS "" CACHE STRING "Ninja arguments to pass before compiling WebRTC")
+option(BUILD_SHARED_LIB "Build WebRTC as a shared library." OFF)
+
+if(BUILD_SHARED_LIB)
+  set(LIBRARY_TYPE SHARED)
+else()
+  set(LIBRARY_TYPE STATIC)
+endif()
+
+#
+# Offer the user the choice of overriding the installation directories
+set(INSTALL_LIB_DIR lib CACHE PATH "Installation directory for libraries")
+set(INSTALL_BIN_DIR bin CACHE PATH "Installation directory for executables")
+set(INSTALL_INCLUDE_DIR include CACHE PATH "Installation directory for header files")
+set(INSTALL_CMAKE_DIR lib/cmake/LibWebRTC CACHE PATH "Installation directory for CMake files")
+
+#
+# Make relative paths absolute (needed later on)
+foreach(p LIB BIN INCLUDE CMAKE)
+  set(var INSTALL_${p}_DIR)
+  if(NOT IS_ABSOLUTE "${${var}}")
+    set(${var} "${CMAKE_INSTALL_PREFIX}/${${var}}")
+  endif()
+endforeach()
