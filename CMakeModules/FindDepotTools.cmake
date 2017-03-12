638022675f26922a19756165fb6c4ed596d9cead
Axel Isouard <axel@isouard.fr>
2017-03-12 12:29:01 +0100

    Look for depot_tools only when DEPOT_TOOLS_PATH is set

--- CMakeModules/FindDepotTools.cmake
+++ CMakeModules/FindDepotTools.cmake
@@ -1,16 +1,17 @@
 find_program(GCLIENT_EXECUTABLE
              NAMES gclient gclient.bat
-             PATHS ${CMAKE_CURRENT_SOURCE_DIR}/depot_tools)
+             DOC "Path to gclient executable"
+             HINTS ${DEPOT_TOOLS_PATH} ENV DEPOT_TOOLS_PATH)
 
-find_path(DEPOTTOOLS_PATH
+find_path(DEPOT_TOOLS_PATH
           NAMES gclient gclient.py gclient.bat
-                gn gn.py gn.bat
                 ninja ninja.exe ninja-linux32 ninja-linux64 ninja-mac
                 download_from_google_storage download_from_google_storage.bat
                 download_from_google_storage.py
-          HINTS ${CMAKE_CURRENT_SOURCE_DIR}/depot_tools)
+          DOC "Path to depot_tools directory"
+          HINTS ${DEPOT_TOOLS_PATH} ENV DEPOT_TOOLS_PATH)
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(DepotTools
-                                  REQUIRED_VARS GCLIENT_EXECUTABLE
-                                  FAIL_MESSAGE "Could not find the gclient executable.")
+                                  REQUIRED_VARS GCLIENT_EXECUTABLE DEPOT_TOOLS_PATH
+                                  FAIL_MESSAGE "Could not find depot_tools.")
